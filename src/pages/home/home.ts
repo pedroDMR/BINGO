707ee3882457021bd2e1da1493868c0f3afd584a
Aarón López Sosa <alsvader@hotmail.com>
2017-08-23 11:18:54 -0500

    Se inició el proyecto de Bingo

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,60 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, IonicPage, AlertController, LoadingController } from 'ionic-angular';
 
+@IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public users: Array<any> = [
+    {name: 'Aarón López Sosa', img: 'assets/img/user_avatar.png'},
+    {name: 'Aarón López Sosa', img: 'assets/img/user_avatar.png'},
+    {name: 'Aarón López Sosa', img: 'assets/img/user_avatar.png'},
+    {name: 'Aarón López Sosa', img: 'assets/img/user_avatar.png'},
+    {name: 'Aarón López Sosa', img: 'assets/img/user_avatar.png'},
+    {name: 'Aarón López Sosa', img: 'assets/img/user_avatar.png'},
+    {name: 'Aarón López Sosa', img: 'assets/img/user_avatar.png'},
+    {name: 'Aarón López Sosa', img: 'assets/img/user_avatar.png'},
+    // {name: 'Aarón López Sosa', img: 'assets/img/user_avatar.png'},
+    // {name: 'Aarón López Sosa', img: 'assets/img/user_avatar.png'},
+    // {name: 'Aarón López Sosa', img: 'assets/img/user_avatar.png'},
+  ]
+
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController, public loadingCtrl: LoadingController) {
 
   }
 
+  checkUser(item): void {
+    let alert = this.alertCtrl.create({
+      title: 'Bingo',
+      message: '¿Confirmar voto?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          handler: data => { console.log('Cancelado'); }
+        },
+        {
+          text: 'Aceptar',
+          handler: data => { 
+            this.showLoading();
+            var index = this.users.indexOf(item);
+            console.log(this.users);
+            this.users.splice(index, 1);
+            console.log(this.users);
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
+  private showLoading() {
+    let loading = this.loadingCtrl.create({
+      content: 'Cargando...',
+      duration: 1000
+    });
+    loading.present();
+  }
 }
